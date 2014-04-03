3e8109e1d0b125cc4f243185ab0712b22e13d3ac
Jaume Mir <jsoler@hacktivisme.org>
2014-04-03 23:43:21 +0200

    Stable version 1.0

--- createmagnets.sh
+++ createmagnets.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-### Create recursive magnets URLs on a directory with rhash package
-
-# http://sourceforge.net/apps/mediawiki/rhash/index.php?title=Usage_examples
-# http://earthwithsun.com/questions/628152/create-and-seed-a-torrent-from-scratch-via-magnet
+# Code from 44Kbps
+# Thanks for the inspitation and the some code @ioerror
+# Thanks to Jimmics for the $(command)
 
 # Necessary package: rhash https://github.com/rhash/RHash
 
-### Bugs-Features ####################
-### 1) No newline on rhash command ###
-### ##################################
-
-# Usage: 
+# rhash Usage: 
 # 1) Direct Connect TTH and EDonkey200: rhash --magnet files
 # 2) Magnet link for Bitorrent P2P Bittorrent Info Hash (BTIH): rhash --magnet --btih files 
 # 3) All supported magnet Hash: rhash -ma files
 
-###TESTING ON rTorret CLIENT, OPTION 3) and 1) NOT WORKING, WRONG URL
+### Bugs-Features
+# 1) More Trackers
+
+###TESTED ON rTorret CLIENT, OPTION 3) and 1) NOT WORKING, WRONG URL
 
 TRACKERS="udp://tracker.openbittorrent.com:80/announce,udp://tracker.publicbt.com:80/announce,http://tracker.openbittorrent.com:80/announce,http://tracker.publicbt.com:80/announce,udp://tracker.ccc.de/announce"
 ARRAY_TRACK=(${TRACKERS//,/ })
@@ -36,11 +34,10 @@ do
  name=${file##*/}
 
  echo Processing $path/$file
- /usr/bin/rhash $OPTS $file >> $DEST_FILE
+ echo -n $(/usr/bin/rhash $OPTS $file) >> $DEST_FILE
  for i in "${!ARRAY_TRACK[@]}"
   do
    echo -n "&tr=${ARRAY_TRACK[i]}" >> $DEST_FILE
   done
- echo
+  echo "" >> $DEST_FILE
 done
-
