6f16e5aadf97c5d61ec8ce01fcc7b0faa1760aee
44Kbps <jsoler@hacktivisme.org>
2014-04-04 14:17:24 +0200

    Update createmagnets.sh

--- createmagnets.sh
+++ createmagnets.sh
@@ -2,7 +2,7 @@
 
 # createmagnets.sh v1.0 GPLv3 by 44Kbps
 # Thanks for the inspitation and the some code @ioerror
-# Thanks to Jimmics for the $(command)
+# Thanks to Jimics for the $(command)
 
 # Necessary package: rhash https://github.com/rhash/RHash
 
